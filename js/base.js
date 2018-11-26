c9454577cbe7dbdf2c9115286748c8238dc18884
QuinJetZhou <zwq0807@gmail.com>
2018-11-26 21:47:41 +0800

    first day

---
+++ js/base.js
@@ -0,0 +1,10 @@
+/*
+* @Author: ZHOU
+* @Date:   2018-11-26 15:12:43
+* @Last Modified by:   ZHOU
+* @Last Modified time: 2018-11-26 15:27:10
+*/
+;(function(){
+    'use strict';
+    // console.log('jquery',jQuery);
+})();
