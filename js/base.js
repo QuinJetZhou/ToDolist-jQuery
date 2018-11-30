4787ca7f4167c42cb71aae47cdb2b7095be27c52
QuinJetZhou <zwq0807@gmail.com>
2018-11-30 20:49:54 +0800

    增加详细功能

--- js/base.js
+++ js/base.js
@@ -2,22 +2,25 @@
  * @Author: ZHOU
  * @Date:   2018-11-26 15:12:43
  * @Last Modified by:   QuinJetZhou
- * @Last Modified time: 2018-11-30 17:18:31
+ * @Last Modified time: 2018-11-30 20:48:47
  */
 ;
 (function() {
     'use strict';
 
     var $form_add_task = $('.add-task'),
-        $delete_task,
+        $task_delete_trigger,
+        $task_detail_trigger,
+        $task_detail = $('.task-detail'),
+        $task_detail_mask = $('.task-detail-mask'),
         task_list = [];
-
 
 
 
     init();
 
-    $form_add_task.on('submit',on_add_task_form_submit)
+    $form_add_task.on('submit', on_add_task_form_submit);
+    $task_detail_mask.on('click',hide_task_detail);
 
     function on_add_task_form_submit(e) {
         var new_task = {},
@@ -34,20 +37,63 @@
             // render_task_list();
             $input.val(null);
         }
+    }
 
+    function listen_task_detail() {
+        $task_detail_trigger.on('click', function() {
+            var $this = $(this);
+            var $item = $this.parent().parent();
+            var index = $item.data('index');
+            console.log(index);
+            show_task_detail(index);
+        })
+    }
 
+    function show_task_detail(index){
+        render_task_detail(index);
+        $task_detail.fadeIn();
+        $task_detail_mask.fadeIn();
+    }
+
+    function hide_task_detail(index){
+        $task_detail.fadeOut() ;
+        $task_detail_mask.fadeOut() ;
+    }
+    //渲染指定task的详细信息
+    function render_task_detail(index){
+        if(index === undefined || !task_list[index])
+            return;
+
+        var item = task_list[index];
+        var tpl=
+        '<form>'+
+        '<div class="content">'+
+                item.content +
+        '</div>'+
+        '<div>'+
+            '<div class="decs">'+
+                '<textarea "'+item.desc+'"></textarea>'+
+            '</div>'+
+        '</div>'+
+        '<div class="remind">'+
+            '<input type="date">'+
+        '</div>'+
+        '</form>';
+
+        $task_detail.html(null);
+        $task_detail.html(tpl);
     }
 
     //查找并监听所有删除按钮的点击事件
     function listen_task_delete() {
-        $delete_task.on('click', function() {
+        $task_delete_trigger.on('click', function() {
             var $this = $(this);
             //找到删除按钮所在的task元素
             var $item = $this.parent().parent();
             var index = $item.data('index');
             //确认删除
             var tmp = confirm('即将删除！');
-            tmp ? delete_task(index) : null;
+            tmp ? task_delete(index) : null;
         })
     }
 
@@ -72,7 +118,7 @@
     }
 
     // 删除一条task
-    function delete_task(index) {
+    function task_delete(index) {
         //如果没有index 或者index不存在则直接返回
         if (index == undefined || !task_list[index]) return;
 
@@ -95,8 +141,10 @@
             var $task = render_task_item(task_list[i], i);
             $task_list.append($task)
         }
-        $delete_task = $('.action.delete');
+        $task_delete_trigger = $('.action.delete');
+        $task_detail_trigger = $('.action.detail');
         listen_task_delete();
+        listen_task_detail();
     }
 
     //渲染单条task模板
@@ -108,7 +156,7 @@
             '<span class="task-content">' + data.content + '</span>' +
             '<span class="fr">' +
             '<span class="action delete"> 删除</span>' +
-            '<span class="action"> 详细</span>' +
+            '<span class="action detail"> 详细</span>' +
             '</span>'
         '</div>';
         return $(list_item_tpl);
